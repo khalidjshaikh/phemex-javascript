6bcae539b7268d47740d6edb5158e4cdef4c472f
Khalid Shaikh <k@ruby.run>
2026-07-10 22:17:00 -0500

    u

--- phemex-create-limit-orders.rb
+++ phemex-create-limit-orders.rb
@@ -35,7 +35,7 @@ orders=[]
 # 1.times{|i| p i; orders << [:XTIUSDT, :Long, 71 + i, 0.01, 100]}
 # 21.times{|i| p i; orders << [:XTIUSDT, :Long, 50 + i, 0.01, 100]}
 # 3.times{|i| orders << [:XTIUSDT, :Long, 70 - i, 0.01, 100]}
-4.times{|i| orders << [:XTIUSDT, :Short, 80 + i, 0.01, 100]}
+4.times{|i| orders << [:XTIUSDT, :Short, 80 + 1e1 * i, 0.01, 100]}
 
 orders.each do |symbol, side, price, qty, leverage|
   s = "./phemex-create-limit-order.ts --account coin-m --symbol #{symbol} --side #{side} --price #{price} --qty #{qty} --leverage #{leverage}" if symbol.to_sym == :BTCUSD
