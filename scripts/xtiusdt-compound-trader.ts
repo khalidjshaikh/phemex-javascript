25a0f61f7279e60eca624ba545391ee811ef7683
Khalid Shaikh <k@ruby.run>
2026-08-16 12:43:07 -0500

    u

--- scripts/xtiusdt-compound-trader.ts
+++ scripts/xtiusdt-compound-trader.ts
@@ -513,8 +513,8 @@ function algoMomentumVolume(price: number, ind: Indicators): AlgorithmSignal {
 }
 
 function algoIndexTrade(index: number, last: number): AlgorithmSignal {
-  const bias = -0.3555;
-  const threshold = 0.4;
+  const bias = -0.1;
+  const threshold = 0.1;
   const indexLast = index - last;
 
   if (!Number.isFinite(indexLast)) {
