0f26f49a015eec5ba14fc440a1d4fd9f93fe2616
root <k@iai.lol>
2026-08-17 08:53:39 -0500

    u

--- trader.sh
+++ trader.sh
@@ -1,1 +1,3 @@
-scripts/xtiusdt-compound-trader.ts --restore 2>&1 | tee -a "trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+#!/bin/bash
+# scripts/xtiusdt-compound-trader.ts --restore "$@" 2>&1 | tee -a "trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+npx tsx scripts/phemex-ws-auto-trader.ts "$@" --qty 0.01 | tee -a "trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
