94e63112bdb0ae9d49463f8545a73d13d9591ec7
Khalid Shaikh <k@iai.lol>
2026-08-18 12:14:37 -0500

    u

--- trader2.sh
+++ trader2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scripts/phemex-ws-auto-trader2.ts "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential A02 --configfile config/config.json5| tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential A02 --configfile config/config.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
