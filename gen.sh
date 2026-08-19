007c977a162e59746ae276fa2a01dd424b0feb1a
Khalid Shaikh <k@iai.lol>
2026-08-19 12:48:19 -0500

    u

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-scripts/generate_config.rb -m 1.2
-scripts/generate_config.rb -m 2 -o config/67b.json5
+scripts/generate_config.rb -m 2
+scripts/generate_config.rb -m 3 -o config/67b.json5
