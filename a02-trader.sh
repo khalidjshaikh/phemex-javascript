4484b27778f83ec27d54672afc05bd549486398c
Khalid Shaikh <k@iai.lol>
2026-08-19 17:47:31 -0500

    u

--- a02-trader.sh
+++ a02-trader.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 6 "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential A02 --configfile config/a02.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 2 "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential A02 --configfile config/a02.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
