f5636b2890ef0e576392871eb7dcbb90cbb3d41d
Khalid Shaikh <k@iai.lol>
2026-08-19 18:09:54 -0500

    u

--- meta-trader.sh
+++ meta-trader.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # scripts/xtiusdt-compound-trader.ts --restore "$@" 2>&1 | tee -a "trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
-#npx tsx scripts/phemex-ws-auto-trader2.ts "$@" --symbol XTIUSDT,XBRUSDT,XAUUSDT | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
-scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 2 "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+# npx tsx scripts/phemex-ws-auto-trader2.ts "$@" --symbol XTIUSDT,XBRUSDT,XAUUSDT | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+# scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 2 "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 6 "$@" --symbol XAUUSDT,XBRUSDT,XTIUSDT --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
 
