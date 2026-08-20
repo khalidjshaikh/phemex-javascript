d98ab88bd2f76df5b4ba029f1b466670f8c2cd02
Khalid Shaikh <k@iai.lol>
2026-08-20 18:18:54 -0500

    u

--- gm-trader.sh
+++ gm-trader.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-scripts/trader2.ts --configfile config/gm.json5 --credential gmail --symbols XTIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XAUUSDT6,XAUUSDT5
+scripts/trader2.ts --configfile config/gm.json5 --credential gmail --symbols XTIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XAUUSDT6,XAUUSDT5,XAUUSDT4
 # scripts/trader2.ts --configfile config/gm.json5 --credential gmail 
 # scripts/trader2.ts --configfile config/gm.json5 --credential gmail --signalExit
