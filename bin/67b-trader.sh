b5b3e57fc9c9bf712c019b9d26539525b45a946f
Khalid Shaikh <k@iai.lol>
2026-08-20 22:12:06 -0500

    u

--- bin/67b-trader.sh
+++ bin/67b-trader.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 6 "$@" --symbols BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential 67b --configfile config/67b.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential 67b --configfile config/67b.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
