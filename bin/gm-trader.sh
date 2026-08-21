ab51e2598b095c643fc90560d30897ee0c1a3211
Khalid Shaikh <k@iai.lol>
2026-08-21 04:28:03 -0500

    Update trader symbols and XAUUSDT10/11 thresholds

--- bin/gm-trader.sh
+++ bin/gm-trader.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-scripts/trader2.ts --configfile config/gm.json5 --credential gmail --symbols XTIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XAUUSDT6,XAUUSDT5,XAUUSDT
+scripts/trader2.ts --configfile config/gm.json5 --credential gmail --symbols XTIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XAUUSDT,XAUUSDT10,XAUUSDT11,XAUUSDT8_6to8,XTIUSDT4_6to8
 # scripts/trader2.ts --configfile config/gm.json5 --credential gmail 
 # scripts/trader2.ts --configfile config/gm.json5 --credential gmail --signalExit
--- bin/meta-trader.sh
+++ bin/meta-trader.sh
@@ -3,4 +3,4 @@
 # npx tsx scripts/phemex-ws-auto-trader2.ts "$@" --symbol XTIUSDT,XBRUSDT,XAUUSDT | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
 # scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 2 "$@" --symbol BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
 # scripts/phemex-ws-auto-trader2.ts --no-reversal-exit --error-budget 6 "$@" --symbols XAUUSDT,XBRUSDT,XTIUSDT,XRPUSDT --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
-scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols XAUUSDT,XBRUSDT,XTIUSDT,XRPUSDT,XAUUSDT7 --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols XTIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XAUUSDT,XAUUSDT10,XAUUSDT11,XAUUSDT8_6to8,XTIUSDT4_6to8 --credential meta --configfile config/gm.json5 | tee -a "logs/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
