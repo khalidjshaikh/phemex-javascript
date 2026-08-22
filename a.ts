d92ed4d7720e7a7f061e818aa53bd61de8d71c6b
Khalid Shaikh <k@iai.lol>
2026-08-22 13:32:34 -0500

    chore: gitignore vector state files, update a.ts scratch

--- a.ts
+++ a.ts
@@ -1,16 +1,13 @@
-const filenames = [
-  "ask.txt",
-  "bid.txt",
-  "index.txt",
-  "indexLast.txt",
-  "last.txt",
-  "mark.txt",
-  "markLast.txt",
-];
-
-console.log(filenames)
-
-const symbol = "BTCUSDT"
-const prefixed = filenames.map((filename) => `${symbol}-${filename}`);
-
-console.log(prefixed);
+import { publicGet } from "./src/http-client.js";
+async function main() {
+  const resp = await publicGet("/public/products", null);
+  const products = (resp.data?.perpProductsV2 ?? []);
+  const p = products.find(x => x.symbol === "BTCUSDT");
+  console.log(JSON.stringify(Object.keys(p), null, 2));
+  console.log("---");
+  console.log("maxLeverage:", p?.maxLeverage);
+  console.log("maxPositionValue:", p?.maxPositionValue);
+  console.log("initialMargin:", p?.initialMargin);
+  console.log("maintenanceMargin:", p?.maintenanceMargin);
+}
+main();
