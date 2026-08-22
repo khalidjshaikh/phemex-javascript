141b8fc10598a9df842b21ded9e34407c65b7b52
Khalid Shaikh <k@iai.lol>
2026-08-22 09:29:48 -0500

    u

--- bin/67b-trader.sh
+++ bin/67b-trader.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential 67b --configfile config/67b.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+# scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential 67b --configfile config/67b.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols XRPUSDT --credential 67b --configfile config/67b.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
--- bin/a02-trader.sh
+++ bin/a02-trader.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential A02 --configfile config/a02.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+# scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols BNBUSDT,BTCUSDT,DOGEUSDT,ETHUSDT,LINKUSDT,SOLUSDT,SUIUSDT,XAUUSDT,XBRUSDT,XRPUSDT,XTIUSDT --credential A02 --configfile config/a02.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
+scripts/phemex-ws-auto-trader2.ts --no-reversal-exit "$@" --symbols XRPUSDT,XTIUSDT,XBRUSDT,ETHUSDT,BTCUSDT --credential A02 --configfile config/a02.json5 --no-ticker-logs | tee -a "logs2/trader-$(date '+%Y-%m-%d_%H-%M-%S').log"
